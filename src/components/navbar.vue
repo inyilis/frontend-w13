8fd98094b3271f3b59e97cbb5103d1f5f2e02dc7
Inyilis Punya <inyilispunya@gmail.com>
2021-02-12 15:04:01 +0700

    up nav

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -10,24 +10,24 @@
     </button>
     <div class="dropdown-menu text-center" aria-labelledby="dropdownMenuButton">
       <div  v-if="getAuth.role == 'admin' || getAuth.role == 'user'" class="dropdown-item my-4">
-        <router-link to="/home" class="dropdown-item my-4">
+        <router-link to="/inyil/home" class="dropdown-item my-4">
           <img src="../assets/fork.png" alt="" />
         </router-link>
       </div>
       <div  v-if="getAuth.role == 'admin' || getAuth.role == 'user'" class="dropdown-item my-4">
-        <router-link to="/history" class="dropdown-item my-4">
+        <router-link to="/inyil/history" class="dropdown-item my-4">
           <img src="../assets/clipboard.png" alt="" />
         </router-link>
       </div>
       <div  v-if="getAuth.role == 'admin'" class="dropdown-item my-4">
-        <router-link to="/products">
+        <router-link to="/inyil/products">
           <img src="../assets/add.png" alt="" />
         </router-link>
       </div>
       <!-- <router-link to="/products" class="dropdown-item my-4">
         <img src="../assets/profil.png" alt="" />
       </router-link> -->
-      <router-link to="/login" class="dropdown-item my-4">
+      <router-link to="/inyil/login" class="dropdown-item my-4">
         <img src="../assets/exit.png" alt="" @click="logout() & cartNull()" />
       </router-link>
     </div>
